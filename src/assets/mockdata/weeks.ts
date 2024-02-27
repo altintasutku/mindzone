de4815232c41b0a2c377685ae7e0e442745d2138
altintasutku <altintasutku@gmail.com>
2024-02-28 00:59:53 +0300

    mock data changed

--- src/assets/mockdata/weeks.ts
+++ src/assets/mockdata/weeks.ts
@@ -2,17 +2,17 @@ export const weeks: WeekProgress[] = [
   {
     done: false,
     locked: false,
-    progress: 100,
-  },
-  {
-    done: true,
-    locked: false,
-    progress: 100,
+    progress: 80,
   },
   {
     done: false,
-    locked: false,
-    progress: 20,
+    locked: true,
+    progress: 0,
+  },
+  {
+    done: false,
+    locked: true,
+    progress: 0,
   },
   {
     done: false,
