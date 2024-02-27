950a54f95c48f954999aa91d711d7a2a25f3fcc2
altintasutku <altintasutku@gmail.com>
2024-02-28 00:49:33 +0300

    week 1 dashboard

--- src/app/week/1/page.tsx
+++ src/app/week/1/page.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const WeekOnePage = () => {
   return (
-    <>
-
-    </>
+    <div className="text-center">
+      Hafta 1&apos;e hoşgeldiniz!
+      <br />
+      Aşağıdaki oyunlardan birini seçebilirsiniz:
+    </div>
   );
 };
 
