020a9878b863b239eb62072b1c6b61fd2f355333
altintasutku <altintasutku@gmail.com>
2024-02-28 00:56:58 +0300

    responsive fix

--- src/components/home/ContactAndAboutButtons.tsx
+++ src/components/home/ContactAndAboutButtons.tsx
@@ -1,28 +1,23 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { Separator } from "../ui/separator";
 
 const ContactAndAboutButtons = () => {
   return (
-    <section className="flex justify-center text-center bg-white p-4 shadow-md rounded-md w-full">
-      <div>
-        <small className="text-sm text-center opacity-65">
-          Daha fazla bilgi almak için bize ulaşın
-        </small>
-        <br />
-        <Button asChild variant={"link"} className="underline">
-          <Link href="/contact">İletişim</Link>
-        </Button>
-      </div>
-      <div>
-        <small className="text-sm text-center opacity-65">
-          ya da hakkımızda daha fazla bilgi edinin
-        </small>
-        <br />
-        <Button asChild variant={"link"} className="underline">
-          <Link href="/about">Hakkında</Link>
-        </Button>
-      </div>
+    <section className="grid grid-cols-2 justify-center text-center bg-white p-4 shadow-md rounded-md w-full">
+      <small className="text-sm text-center opacity-65">
+        Daha fazla bilgi almak için bize ulaşın
+      </small>
+      <small className="text-sm text-center opacity-65">
+        ya da hakkımızda daha fazla bilgi edinin
+      </small>
+      <Button asChild variant={"link"} className="underline">
+        <Link href="/contact">İletişim</Link>
+      </Button>
+      <Button asChild variant={"link"} className="underline">
+        <Link href="/about">Hakkında</Link>
+      </Button>
     </section>
   );
 };
