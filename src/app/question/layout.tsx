927a8d86eedd2c94340ff317f2256ef55ab0151a
altintasutku <altintasutku@gmail.com>
2024-02-29 12:01:51 +0300

    question pages added

---
+++ src/app/question/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = Readonly<{
+  children: React.ReactNode;
+}>;
+
+const QuestionLayout = ({ children }: Props) => {
+  return (
+    <div className="flex justify-center items-center py-5">
+      <div className="bg-white p-5 shadow rounded-md">{children}</div>
+    </div>
+  );
+};
+
+export default QuestionLayout;
