494cb79910c083dfeb3ec368a7cee5ca902e13b1
Lodarks <ahmedyusufsirin@gmail.com>
2024-03-02 20:09:17 +0300

    week1 affective emphaty done

---
+++ src/app/week/1/affective-empathy/_intorductions.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const IntroductionCF = () => {
+  return (
+    <>
+      <p>
+        Ekranda farklı yüz resimleri göreceksiniz. Burada sizden istenen aynı
+        kişileri eşleştirmeniz ya da duyguları eşleştirmenizdir. İlk başta aynı
+        kişinin yüzünü seçmeniz beklenmektedir. Hadi başlayalım!
+        <br />
+      </p>
+    </>
+  );
+};
+
+export default IntroductionCF;
