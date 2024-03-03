7d4d7318aea66eec72b460410c8463e8f0e2ef6f
altintasutku <altintasutku@gmail.com>
2024-03-03 18:38:43 +0300

    director task

---
+++ src/app/week/1/director-task/_introductions.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DirectorTaskIntroductions = () => {
+  return (
+    <p>
+      Ekrandaki rafların karşısında “yönetmen” olduğu belirtilen kişiyi göreceksiniz. Yönetmen de siz de rafları ve raflardaki nesneleri görebilmektesiniz.
+      <br />
+      Tek farkı, yönetmen bazı rafları görememektedir. Arkası kapalı olan rafları yalnızca siz görebilmektesiniz.
+      <br />
+      Sizden istenen “yönetmenin” istediği nesneleri seçmenizdir.
+      <br />
+      Hadi başlayalım!
+    </p>
+  )
+}
+
+export default DirectorTaskIntroductions
