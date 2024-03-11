5a8eb9742735947d8f65f70f09b4aecd5b08a779
Lodarks <ahmedyusufsirin@gmail.com>
2024-03-11 20:24:24 +0300

    weektwogamefive

---
+++ src/app/week/2/affective-empathy/_introductions.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WeekTwoGameFourIntroductions = () => {
+  return <p>Ekranda her soru için bir yüz fotoğrafı göreceksiniz. Sizlerden bu fotoğraf aynı yüz olan kişiyle eşleştirmenizi istiyoruz. </p>;
+};
+
+export default WeekTwoGameFourIntroductions;
