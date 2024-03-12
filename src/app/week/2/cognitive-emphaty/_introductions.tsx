e88a80c89c5064369e96e7ec4772fda82fd54b22
Lodarks <ahmedyusufsirin@gmail.com>
2024-03-12 22:40:19 +0300

    little update

--- src/app/week/2/cognitive-emphaty/_introductions.tsx
+++ src/app/week/2/cognitive-emphaty/_introductions.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const WeekTwoGameFourIntroductions = () => {
   return (
-    <p>
-      <h2 className="text-xl font-semibold text-center">
+    <div>
+      <h2 className='text-xl font-semibold text-center'>
         4. egzersiz açıklaması
       </h2>
       ŞİMDİ SİZLERE SOSYAL YAŞAMIMIZDA KARŞILAŞTIĞIMIZ BAZI KISA SENARYOLARI
       GÖSTERECEĞİZ. BU SENARYOLARI DİKKATLİCE OKUYUN VE EN UYGUN CEVABI
       İŞARETLEYİN.
-    </p>
+    </div>
   );
 };
 
