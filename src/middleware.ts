5d8427ee929e71415c6799b584f49402eb51064d
altintasutku <altintasutku@gmail.com>
2024-03-13 13:58:44 +0300

    m

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
-
-  console.log("token", token);
-  console.log("req", req);
+  // const token = await getToken({ req });
+  
+  const token = req.cookies.get("next-auth.session-token")?.value;
 
   if (!token && req.nextUrl.pathname.includes("/dashboard")) {
     return NextResponse.redirect(new URL("/register", req.nextUrl));
