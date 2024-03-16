cc017160c60e87fffa0dc390e8230c76a5e528b3
altintasutku <altintasutku@gmail.com>
2024-03-16 12:40:46 +0300

    routing changes

--- src/app/(dashboard)/week/2/inhibition/_introductions.tsx
+++ src/app/(dashboard)/week/2/inhibition/_introductions.tsx
@@ -3,12 +3,14 @@ import React from "react";
 const WeekTwoGameThreeIntroduction = () => {
   return (
     <div>
-    <h2 className="text-xl font-semibold text-center">
-      3. egzersiz açıklaması
-    </h2>
-      Ekran&apos;da farklı duygularda fotoğraflar göreceksiniz{" "}
-      <strong>mutlu</strong> yüzleri görünce GİT tuşuna basmanız gerekmektedir.
-      Hadi başlayalım!
+      <h2 className="text-xl font-semibold text-center">
+        3. egzersiz açıklaması
+      </h2>
+      <p>
+        Ekran&apos;da farklı duygularda fotoğraflar göreceksiniz{" "}
+        <strong>mutlu</strong> yüzleri görünce GİT tuşuna basmanız
+        gerekmektedir. Hadi başlayalım!
+      </p>
     </div>
   );
 };
--- src/app/(dashboard)/week/2/working-memory/_introductions.tsx
+++ src/app/(dashboard)/week/2/working-memory/_introductions.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const IntroductionCF = () => {
   return (
-    <>
+    <div>
+      <h2 className="text-xl font-semibold text-center">
+        1. egzersiz açıklaması
+      </h2>
       <p>
-        <h2 className="text-xl font-semibold text-center">
-          1. egzersiz açıklaması
-        </h2>
         EKRANDA BAZI İNSAN YÜZLERİ GÖRECEKSİNİZ. AYNI KİŞİNİN YÜZÜNÜ
         EŞLEŞTİRMENİZ BEKLENMEKTEDİR. HADİ BİRAZ ISINALIM!
-        <br />
       </p>
-    </>
+      <br />
+    </div>
   );
 };
 
