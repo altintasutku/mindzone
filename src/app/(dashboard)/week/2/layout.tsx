4c6c641014ecfa373d97473cb84ff4ee137ccc6d
altintasutku <altintasutku@gmail.com>
2024-04-02 13:48:39 +0300

    ss

--- src/app/(dashboard)/week/2/layout.tsx
+++ src/app/(dashboard)/week/2/layout.tsx
@@ -3,12 +3,7 @@ import WeekContainer from "@/components/game/WeekContainer";
 import React from "react";
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return (
-    <div className="mx-auto my-10 p-10 rounded-md bg-white dark:bg-zinc-900 max-w-[600px] font-semibold text-lg">
-      Hafta iki çalışma altında yarın tekrar deneyin.
-    </div>
-  );
-  // return <WeekContainer games={weekTwoGames}>{children}</WeekContainer>;
+  return <WeekContainer games={weekTwoGames}>{children}</WeekContainer>;
 };
 
 export default Layout;
