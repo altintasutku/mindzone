f5a1284ddfdb06ffad9c2af27c8f97610485131a
Lodarks <ahmedyusufsirin@gmail.com>
2024-04-27 01:20:37 +0300

    game 5 eklensinke

---
+++ src/assets/mockdata/weekGames/week4game5data.ts
@@ -0,0 +1,182 @@
+export const week4Game5Questions = [
+  {
+    question:
+      "Murat’ın çalışma arkadaşı önemli bir bilgiyi zamanında teslim edemeyerek Murat’ın da programın gerisinde kalmasına neden olur. Murat’a hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Kızgın", "2) Mutlu", "3) İğrenmiş", "4) Korkmuş"],
+    answer: "1",
+  },
+  {
+    question:
+      "Rana tam zamanlı anne olmak için işinden ayrılmıştır ve bunu çok sevmektedir ancak iş arkadaşlarının dostluğunu ve şirketini özlemektedir. Rana’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) İğrenmiş", "2) Mutlu", "3) Üzgün", "4) Korkmuş"],
+    answer: "3",
+  },
+  {
+    question:
+      "Zeynep, iş arkadaşlarının sahip olmadığı belirli becerilere sahiptir ve bu nedenle iş yükünün daha fazla olduğunu hissetmektedir ve bu yüzden artık çalışamayacağını ve yetersiz olduğunu düşünmeye başlamıştır. Zeynep’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Üzgün", "2) Korkmuş", "3) Şaşkın", "4) Nötr"],
+    answer: "1",
+  },
+  {
+    question:
+      "Emine, yeni bir çalışan olan Metin’e sistemin nasıl çalıştığını göstermektedir. Emine’nin patronu yanlarına gelir ve değişiklikler yapıldığı için Emine’nin birkaç noktada yanıldığını söyler. Normalde birbirleriyle çok fazla işleri olmamasına rağmen Emine’ye çalışma arkadaşının yanında bu şekilde konuşmasından rahatsız olur. Emine’ye hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Mutlu", "2) Üzgün", "3) Korkmuş", "4) Öfkeli"],
+    answer: "4",
+  },
+  {
+    question:
+      "Mihriban ve Tuba yıllarca aynı ofisi paylaşmışlardır ancak Mihriban yeni bir işe girer ve Tuba onunla iletişimini kaybeder. Tuba’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Korkmuş", "2) Mutlu", "3) İğrenmiş", "4) Üzgün"],
+    answer: "4",
+  },
+  {
+    question:
+      "Mehmet çok rağbet gören bir pozisyon için kabul edilir ve bütün şartlar onun için olumlu görünmektedir. Mehmet’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Şaşkın", "2) Mutlu", "3) Nötr", "4) Korkmuş"],
+    answer: "2",
+  },
+  {
+    question:
+      "Emre, pozisyonunun artık kaldırıldığını öğrendiğinde emekliliğine sadece birkaç yıl kalmıştır. Ancak yine de daha az prestijli bir rolü olan bir işi olacaktır. Emre’nin hissetmesi en muhtemel duygu nedir?",
+    options: ["1) Kızgın", "2) Korkmuş", "3) Üzgün", "4) Mutlu"],
+    answer: "3",
+  },
+  {
+    question:
+      "Ali, ara sıra birlikte çalıştığı akranı Tarık’a zor bir görevde yardım eder. Tarık, Ali’nin yaptığı işin çok iyi olmadığından şikayet eder ve Ali, Tarık’ın ona iyilik yaptığı için minnettar olması gerektiğini söyler. Tartışırlar. Ali’nin hissetmesi en muhtemel duygu nedir?",
+    options: ["1) Şaşkın", "2) Kızgın", "3) İğrenmiş", "4) Korkmuş"],
+    answer: "2",
+  },
+  {
+    question:
+      "Safiye kimseyi tanımadığı yeni bir işe başlar ve kimsenin pek de arkadaş canlısı olmadığını görür. Safiye’nin hissetmesi en muhtemel duygu nedir?",
+    options: ["1) Korkmuş", "2) Üzgün", "3) Mutlu", "4) İğrenmiş"],
+    answer: "2",
+  },
+  {
+    question:
+      "Demir, sunuma yeterince çalışmadığı için bir grup kıdemli kişiye sunum yapmaktan çekiniyor. Demir’in hissetmesi en muhtemel duygu nedir?",
+    options: ["1) Mutlu", "2) Şaşkın", "3) Nötr", "4) Korkmuş"],
+    answer: "4",
+  },
+  {
+    question:
+      "Ayşe, ailesinden ayrılarak birkaç arkadaşıyla kiraladıkları evde yaşamaya başlar. Ancak Ayşe, ev arkadaşının artakalan yemekleri buzdolabında bozulana kadar sakladığını ve dolabın sürekli kötü bir koku yaydığını fark eder. Ayşe'nin bu durum karşısında hissetme olasılığı en yüksek duygu nedir?",
+    options: ["1) Korkmuş", "2) Mutlu", "3) İğrenmiş", "4) Nötr"],
+    answer: "3",
+  },
+  {
+    question:
+      "Helin bir proje üzerinde ekibiyle beraber çalışmaktadır. Helin en iyi sonucu alabilmek için çok çalışmaktadır. Ancak Helin’in ekibindeki diğer arkadaşları projeye gereken önemi göstermez. Helin’in hissetmesi en muhtemel duygu nedir?",
+    options: ["1) Şaşkın", "2) Kızgın", "3) İğrenmiş", "4) Korkmuş"],
+    answer: "2",
+  },
+  {
+    question:
+      "Cenk uzun süredir yurtdışındadır ve ailesini ziyaret etmek için geri döner. O kadar çok şey değişmiştir ki Cenk kendini dışlanmış hisseder. Cenk’in hissetmesi en muhtemel duygu nedir?",
+    options: ["1) Korkmuş", "2) Mutlu", "3) Üzgün", "4) Kızgın"],
+    answer: "3",
+  },
+  {
+    question:
+      "Kerem’in PlayStation konsolunu televizyona bağlaması uzun sürer. Ailesi izlerken kız kardeşi 'Seni aptal, her şeyi yanlış yapıyorsun, oyunu çalıştıramıyor musun?' der. Kerem kız kardeşine ve ailesiyle oldukça yakındır. Kerem’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) İğrenmiş", "2) Üzgün", "3) Korkmuş", "4) Mutlu"],
+    answer: "2",
+  },
+  {
+    question:
+      "Büşra’nın anne ve babası 80'li yaşlarının sonundadır ve şehrin merkeziden uzak bir evde tek başlarına yaşamaktadırlar. Büşra ailesinin yardıma ihtiyacı olduğunu düşünüyor ancak bu konuyu ne zaman açsa ailesi öfkeyle reddediyor. Sonunda Büşra’nın ablası anne ve babasına çok yakın bir eve taşınıyor. Büşra’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Üzgün", "2) Korkmuş", "3) Mutlu", "4) Kızgın"],
+    answer: "3",
+  },
+  {
+    question:
+      "Meryem yaptığı işin en yüksek kalitede olmasıyla gurur duyar. Ortak bir projede, diğer insanlar Meryem’in onların hatalarını düzelteceğini varsayarak berbat bir iş yaparlar. Meryem’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Kızgın", "2) Mutlu", "3) İğrenmiş", "4) Korkmuş"],
+    answer: "1",
+  },
+  {
+    question:
+      "Bilal, yakın olduğu ailesinden farklı bir ülkede prestijli bir pozisyona kabul edilmiştir. Bilal ve karısı taşınmaya değer olduğuna karar verirler. Bilal’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Üzgün", "2) Nötr", "3) Korkmuş", "4) Şaşkın"],
+    answer: "2",
+  },
+  {
+    question:
+      "Ebru’nun bazı ekipmanlarında rutin ayarlamalar yapan bir çalışan, Ebru’yu ekipmanın arızalanmasına neden olmakla suçluyor ve daha dikkati olmazsa bu durumu Ebru’nun kıdemlilerine bildireceğini söylüyor. Ebru’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Nötr", "2) Korkmuş", "3) İğrenmiş", "4) Mutlu"],
+    answer: "2",
+  },
+  {
+    question:
+      "Nehir telefona cevap verir ve yakın akrabalarının hastanede ağır hasta olduğunu duyar. Nehir’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Kızgın", "2) İğrenmiş", "3) Korkmuş", "4) Nötr"],
+    answer: "3",
+  },
+  {
+    question:
+      "Çınar’ın vardiyasının sonunda yerine bakan kadın, mazeret ya da özür bildirmeksizin yirmi dakika geç kalır. Çınar’a hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Mutlu", "2) Nötr", "3) Kızgın", "4) İğrenmiş"],
+    answer: "3",
+  },
+  {
+    question:
+      "Alp tam zamanlı eğitime başladıktan sonra, eskiden oldukça iyi olduğu tenis antrenmanlarına harcadığı zamanı ve parayı karşılayamaz. Tam zamanlı eğitimden hoşlanmasına rağmen antrenmanları özlüyor. Sonunda Alp bir spor bursuna başvuruyor ve tenis eğitimlerini karşılayacak kadar burs alıyor. Alp’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Üzgün", "2) Mutlu", "3) Nötr", "4) Korkmuş"],
+    answer: "2",
+  },
+  {
+    question:
+      "Dilara’nın ev arkadaşı gece geç saatlerde yemek pişirdi ve mutfakta büyük bir dağınıklık bıraktı ve Dilara bunu kahvaltıda fark etti. Dilara’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Kızgın", "2) Korkmuş", "3) Mutlu", "4) Nötr"],
+    answer: "1",
+  },
+  {
+    question:
+      "Fatih birkaç yıl aradan sonra üniversiteye dönmüştür. Etrafı, yetenekleri konusunda kendilerine çok güvenen genç öğrencilerle çevrilidir ve onlarla rekabet edip edemeyeceğinden emin değildir. Fatih’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Nötr", "2) Şaşkın", "3) Korkmuş", "4) İğrenmiş"],
+    answer: "3",
+  },
+  {
+    question:
+      "Gamze ve yakın arkadaşları beraber bir eve taşınıyor. Birbirlerini oldukça iyi tanıyorlar. Fakat, Gamze’nin ev arkadaşı Ayşe’nin evde alışagelmişin dışında bazı rutinler uyguladığını gözlemliyor. Gamze’ye hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Şaşkın", "2) Mutlu", "3) Nötr", "4) Korkmuş"],
+    answer: "1",
+  },
+  {
+    question:
+      "Sarp yeğeniyle aylardır konuşmuyor, oysa küçükken çok yakındılar. Onu arıyor ama sadece beş dakika konuşabiliyor. Sarp’a hangi duygu ile eşlik ederdiniz?",
+    options: ["1) İğrenmiş", "2) Üzgün", "3) Nötr", "4) Korkmuş"],
+    answer: "2",
+  },
+  {
+    question:
+      "Sema, avukat arkadaşının işini halletmesi için hakime rüşvet teklif ettiğini öğrenir. Sema’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Nötr", "2) Mutlu", "3) İğrenmiş", "4) Korkmuş"],
+    answer: "3",
+  },
+  {
+    question:
+      "Batu her zaman belli bir müşteriyle ilgilenmiştir ancak çok karmaşık bir iş için patronu bu görevi başka bir iş arkadaşına verir. Batu patronunun kendisinin önemli işlerin üstesinden gelemeyeceğini düşünüp düşünmediğini merak eder. Batu’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Nötr", "2) Korkmuş", "3) İğrenmiş", "4) Üzgün"],
+    answer: "4",
+  },
+  {
+    question:
+      "Havva, babasının yıllardır yakalandığı bir hastalığın son evreside olduğunu öğrendiğinde yurt dışındadır. Havva’ya hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Kızgın", "2) Nötr", "3) İğrenmiş", "4) Korkmuş"],
+    answer: "4",
+  },
+  {
+    question:
+      "Mine evlere belli bir ücret karşılığına temizlik yapmaya gidiyor. En son aldığı işte evin içinde örümcek ağları olduğunu görüyor, daha önce böyle pis bir eve gitmediğini düşünüyor. Mine’ye hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Korkmuş", "2) İğrenmiş", "3) Nötr", "4) Mutlu"],
+    answer: "2",
+  },
+  {
+    question:
+      "Berrin, seyirciler arasında çok sayıda deneyimli oyuncu varken bir sahneyi canlandıracağı için gergindir. Berrin’e hangi duygu ile eşlik ederdiniz?",
+    options: ["1) Nötr", "2) Mutlu", "3) Şaşkın", "4) Korkmuş"],
+    answer: "4",
+  },
+];
