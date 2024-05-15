2bccb836b35f7a0c1724f9671ed53978473f28b9
altintasutku <altintasutku@gmail.com>
2024-05-15 16:46:47 +0300

    yuyuyu

--- src/app/(dashboard)/finish/page.tsx
+++ src/app/(dashboard)/finish/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const FinishPage = () => {
-  return <div className="h-screen w-screen bg-primary p-4 rounded-md">
-    Tebrikler! Tüm etkinlikleri ve uygulamaları bitirdiniz. İletişim için bekleyin!
-  </div>;
+  return (
+    <div className="h-screen w-screen bg-primary p-4 rounded-md">
+      Tebrikler! Tüm etkinlikleri ve uygulamaları bitirdiniz. İletişim için
+      bekleyin!
+    </div>
+  );
 };
-.
 export default FinishPage;
